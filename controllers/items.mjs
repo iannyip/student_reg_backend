c6e8aaa015dcb6596d232c98242cdf2c59d5e2e7
iannyip <ianyip@nullspace.com.sg>
2021-06-20 15:55:00 +0800

    enabled adding of new credit type. added routes, controller actions

--- controllers/items.mjs
+++ controllers/items.mjs
@@ -37,10 +37,78 @@ export default function initItemsController(db) {
       console.log(error);
     }
   };
+  const createForm = async (request, response) => {
+    try {
+      const formMeta = {
+        title: 'Create Credit Type',
+        notes: 'Add applicable course types after creating credit type',
+        formAction: '/items/new',
+        method: 'post',
+        submitVal: 'Submit',
+        cancelVal: 'Cancel',
+        onCancel: '/items',
+        breadcrumbs: [
+          { text: 'credits', href: '/credits' },
+          { text: 'credit types', href: '/items' },
+          { text: 'new credit type', href: '' },
+        ],
+        fields: [
+          {
+            name: 'name',
+            label: 'Name',
+            type: 'text',
+            placeholder: '4-session package',
+            value: '',
+          },
+          {
+            name: 'creditCount',
+            label: 'No. of Credits',
+            type: 'number',
+            placeholder: '',
+            value: '',
+          },
+          {
+            name: 'price',
+            label: 'Price',
+            type: 'number',
+            placeholder: '',
+            value: '',
+          },
+          {
+            name: 'validity',
+            label: 'Validity (years)',
+            type: 'number',
+            placeholder: 'in years',
+            value: '',
+          },
+        ],
+      };
+      response.render('partial/formTemplate', { form: formMeta });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const create = async (request, response) => {
+    try {
+      const { id } = request.params;
+      const formData = request.body;
+      // console.log(formData);
+      const newItem = await db.Item.create({
+        name: formData.name,
+        creditCount: formData.creditCount,
+        price: formData.price,
+      });
+      response.redirect(`/item/${newItem.id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // return all methods we define in an object
   return {
     index,
     show,
+    createForm,
+    create,
   };
 }
