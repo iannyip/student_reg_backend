347bd7919e9fe708e9cf3eb1a2bedf7abdd3bcd9
iannyip <ianyipsh@gmail.com>
2024-01-26 16:52:12 +0800

    updated config to use env variables

--- config/config.js
+++ config/config.js
@@ -1,20 +1,28 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'iannyip',
-    password: null,
-    database: 'stu_reg_development',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
+    port: process.env.DB_PORT,
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: { // https://github.com/sequelize/sequelize/issues/12083
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    // use_env_variable: 'DATABASE_URL',
+    // dialect: 'postgres',
+    // protocol: 'postgres',
+    // dialectOptions: {
+    //   ssl: { // https://github.com/sequelize/sequelize/issues/12083
+    //     require: true,
+    //     rejectUnauthorized: false,
+    //   },
+    // },
   },
 };
